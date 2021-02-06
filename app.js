8a999221ac7e6749628365d0e664c6414be9f200
Eduardo-22002197 <a22002197@alunos.ulht.pt>
2021-02-06 21:37:03 +0000

    Added some animations to the burger menu button and the menu itself. Corrected some problems on the menu items layout. Redimentioned some elements. Added tablet support.

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-list');
-    const navLinks = document.querySelectorAll('.nav-list li');
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
-    
-    burger.addEventListener('click', () => {
+    burger.addEventListener("click", () => {
         //Toggle Nav
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle("nav-active");
 
         //Animate Links
-        /*
         navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = ' ';
+            if (link.style.animation) {
+                link.style.animation = ""
 
             } else {
-                link.style.animation = `navLinkFade 0.5s ease fowards ${index / 7 + 1.2}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
-        */
-    }); 
+        
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
 }
 
 navSlide();
