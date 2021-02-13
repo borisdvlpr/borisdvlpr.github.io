d80ea89d6d3000ca203fe75f67d43a55e2da8794
Eduardo-22002197 <a22002197@alunos.ulht.pt>
2021-02-13 12:45:37 +0000

    Added some animations to the landing page button. Commented some unecessary JS code.

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ function navSlide() {
 
 navSlide();
 
+/*
 const logo = document.querySelectorAll('.landing-image path');
 
 for(let i = 0; i < logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+*/
